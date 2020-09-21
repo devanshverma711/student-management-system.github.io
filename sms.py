5a54a91aa09cfda534ba159465024f5acfb81b39
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2020-09-21 16:17:13 +0530

    Create sms.py

---
+++ sms.py
@@ -0,0 +1,15 @@
+student = []
+def list():
+    for i in range(1,11):
+        print(i)
+        roll=print(input("Roll no= "))
+        #student.append(roll)
+        prn=print(input("PRN= "))
+        #student.append(prn)
+        name=print(input("Name= "))
+        #student.append(name)
+        sem=print(input("Sem= "))
+        #student.append(sem)
+        print("***********")
+#print(student)
+list()
